c7e322546544419ab6f603fe32802617f28eb62a
gianmillare <gianmillaregsm@gmail.com>
2020-10-12 21:33:29 -0700

    completed the login feature of the app, including the inability to login without username and password in database

--- miserme/app.py
+++ miserme/app.py
@@ -10,26 +10,34 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 conn = sqlite3.connect('registrants.db', check_same_thread=False)
-c = conn.cursor
-
-@app.route("/")
-@login_required
-def index():
-    """ Show the available monthly funds, fixed expenses, and total left """
-    return render_template("index.html")
+c = conn.cursor()
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     """ Login the User """
     
-    # Forget any user_id
     session.clear()
 
     # If the request is GET, return the login.html
     if request.method == "GET":
         return render_template("login.html")
+    # Otherwise, search the inputted username and password inside the registrants.db
     else:
-        return render_template("index.html")
+        # if username or password is empty, return error page
+        if not request.form.get("username") or not request.form.get("password"):
+            return render_template("errorpage.html")
+        # elif look through database for the username and password
+        c.execute("SELECT * FROM registrants WHERE username = :username", {"username": request.form.get("username")})
+        results = c.fetchall()
+
+        # check that the user exists in the database and the password matches
+        if len(results) != 1 or not check_password_hash(results[0]["password"], request.form.get("password")):
+            return render_template("errorpage.html")
+        
+        # recognize the user based on id
+        session["user_id"] = results[0]["id"]
+        return redirect("/")
+        
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -45,9 +53,15 @@ def register():
             username = request.form.get("username")
             hashing = generate_password_hash(request.form.get("password"))
             try:
-                primary_key = c.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, hashing))
+                primary_key = c.execute("INSERT INTO users (username, password, funds) VALUES (?, ?, ?)", (username, hashing,))
             except:
                 return render_template("error_user_exists.html")
             
             session["user_id"] = primary_key
             return redirect("/")
+
+@app.route("/")
+@login_required
+def index():
+    """ Show the available monthly funds, fixed expenses, and total left """
+    return render_template("index.html")
